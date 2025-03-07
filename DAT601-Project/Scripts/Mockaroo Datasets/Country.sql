f3d5a8627cee9af8d58e4a96b0c66c695eed1e42
KyrVorga <rhylei@aureate.dev>
2025-03-08 10:06:37 +1300

    zipped sql files so they stop skewing my coding language stats

--- DAT601-Project/Scripts/Mockaroo Datasets/Country.sql
+++
@@ -1,7 +0,0 @@
-insert into Country
-    (CountryName)
-values
-    ('New Zealand'),
-    ('Australia'),
-    ('United Kingdom'),
-    ('United States');
--- DAT601-Project/Scripts/Mockaroo Datasets/SubscriberType.sql
+++
@@ -1,7 +0,0 @@
-insert into SubscriberType
-    (SubscriptionName)
-values 
-    ('Standard'),
-    ('Gold'),
-    ('Platinum'),
-    ('Super Platinum');
